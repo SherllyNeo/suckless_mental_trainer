dba83292b703f75ce41b49666dd78590038c6537
sherllyneo <sherlly.neomail@gmail.com>
2023-03-10 16:50:15 +0000

    added some safety for user input

--- day_of_any_date.c
+++ day_of_any_date.c
@@ -3,8 +3,21 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 #include "config.h"
 #include "doad_calc.h"
+
+char *strlwr(char *str)
+{
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p) {
+     *p = tolower((unsigned char)*p);
+      p++;
+  }
+
+  return str;
+}
 
 void doad_game(int explain) {
 	int min_year = 1700;
@@ -22,18 +35,29 @@ void doad_game(int explain) {
 	int day = (( (min_day) + (rand() % (max_day - min_day) ) ) % days_in_month) + ( rand() % 2 );
 	printf("\nRandom date: %d-%d-%d\n",year,month,day);
 	int day_code = calculate_day(year,month,day,explain);
-	char* day_of_week = day_name(day_code);
+	char* day_of_week_p = day_name(day_code);
+
 	char user_input[20];
+	char day_of_week[20];
+
 	memset(user_input,'\0',strlen(user_input));
+	memset(day_of_week,'\0',strlen(day_of_week));
+
+	strcpy(day_of_week,day_of_week_p);
+
 	printf("\nPlease type in the day of the week for this date\n");
 	fgets(user_input,sizeof(user_input),stdin);
-	if (strncmp(user_input,day_of_week,strlen(day_of_week)) == 0 ) {
+
+	char* user_input_lower = strlwr(user_input);
+	char* day_of_week_lower = strlwr(day_of_week);
+
+	if (strncmp(user_input_lower,day_of_week_lower,strlen(day_of_week_lower)) == 0 ) {
 		printf(ANSI_COLOR_GREEN "\nyou got it right!\n" ANSI_COLOR_RESET);
 	}
 	else {
 		printf(ANSI_COLOR_RED "\nyou got it wrong! The answer was %s\n" ANSI_COLOR_RESET,day_of_week);
 		}
-	sleep(3);
+	sleep(1);
 	}
 
 }
